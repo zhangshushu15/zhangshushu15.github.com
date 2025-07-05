6b0505954b3b13f14ea7940e9ebdb2c20c097d42
zhangshushu15 <zhangshushu15@gmail.com>
2025-07-05 16:32:35 -0700

    Add basic blog system

---
+++ lib/posts.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { remark } from 'remark';
+import html from 'remark-html';
+
+const postsDirectory = path.join(process.cwd(), 'posts');
+
+export interface PostData {
+  slug: string;
+  title: string;
+  date: string;
+  contentHtml: string;
+}
+
+export function getSortedPostsData() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const matterResult = matter(fileContents);
+    return {
+      slug,
+      ...(matterResult.data as { title: string; date: string }),
+    };
+  });
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
+}
+
+export async function getPostData(slug: string): Promise<PostData> {
+  const fullPath = path.join(postsDirectory, `${slug}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  const matterResult = matter(fileContents);
+
+  const processedContent = await remark().use(html).process(matterResult.content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    slug,
+    contentHtml,
+    ...(matterResult.data as { title: string; date: string }),
+  };
+}
